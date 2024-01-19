ec9b2e501e8294fcc8b557bf8acbfef811ef2591
VidnyaniU <vidnyani.umathe@gmail.com>
2024-01-19 09:14:05 +0530

    Updated reverseLL.cpp
    Added a function to reverse k nodes of linked list.

--- linkedList/reverseLL.cpp
+++ linkedList/reverseLL.cpp
@@ -55,6 +55,31 @@ struct LinkedList
         return newhead;
     }
 
+    // reverse k nodes in a linked list
+    Node *reverseK(Node *&head, int k)
+    {
+        Node *prevptr = NULL;
+        Node *currptr = head;
+        Node *nextptr;
+
+        int count = 0;
+        while (currptr != NULL && count < k) // to reverse k nodes
+        {
+            nextptr = currptr->next;
+            currptr->next = prevptr;
+            prevptr = currptr;
+            currptr = nextptr;
+            count++;
+        }
+        // check  if nextptr is NULL
+        if (nextptr != NULL)
+        {
+            // next k nodes
+            head->next = reverseK(nextptr, k);
+        }
+
+        return prevptr;
+    }
     void push(int data) // insert at beginning
     {
         Node *temp = new Node(data);
@@ -85,7 +110,10 @@ int main(int argc, char const *argv[])
 
     ll.printList();
     // ll.reverseIter();
-    ll.head = ll.reverseRecur(ll.head);
+    // ll.head = ll.reverseRecur(ll.head);
+
+    int k = 2;
+    ll.head = ll.reverseK(ll.head, k);
     ll.printList();
 
     return 0;
